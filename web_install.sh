28ec618f055a88b2960c563199929d5f5fcf4ac2
devnetcoding <github@devnetcoding.ga>
2020-02-01 14:54:16 +0100

    Update web_install.sh

--- web_install.sh
+++ web_install.sh
@@ -38,8 +38,8 @@ unzip phpmyadmin.zip
 rm phpmyadmin.zip
 mv phpMyAdmin-*-all-languages phpmyadmin
 chmod -R 0755 phpmyadmin
+cp phpmyadmin.conf /etc/apache2/conf-available/phpmyadmin.conf
 cd /etc/apache2/conf-available/
-wget https://devnetcoding.ga/data/files/phpmyadmin.conf
 a2enconf phpmyadmin
 systemctl reload apache2
 mkdir /usr/share/phpmyadmin/tmp/
